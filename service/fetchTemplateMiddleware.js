623bc94654acd4cdf12bcf8631868c08265c66f0
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-06-11 19:18:04 -0300

    removido log e comentado url para desenvolvimento.

--- service/fetchTemplateMiddleware.js
+++ service/fetchTemplateMiddleware.js
@@ -4,10 +4,8 @@ const { resolve } = require('path');
 const fetchTemplateMiddleware = async (req, _res, next) => {
     try {
         const { template: templateUrl, data } = req.body;
-        console.log(templateUrl);
         const source = await fetch(templateUrl);
         const template = await source.text();
-        console.log(template);
         const fs = require('fs');
     fs.writeFile(resolve(__dirname, '..', 'views', 'layouts', 'main.hbs'), template, function(err) {
     if(err) {
