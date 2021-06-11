d0872e8d193fac77ff7733b36893d0e586a6d36b
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-06-11 18:16:12 -0300

    remoção de comentários, logs e adição de script start

--- routes.js
+++ routes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const route = express.Router();
-// const { resolve } = require('path');
 
 const { retornaCertificadoController, renderizaCertificadoController } = require('./controller/certificadoController');
 const { validateRequestMiddleware } = require('./service/validateRequestMiddleware');
 const { fetchTemplateMiddleware } = require('./service/fetchTemplateMiddleware');
-const { renderizaCertificadoMiddleware } = require('./service/renderizaCertificadoMiddleware');
 
 route.post(
     '/certificado',
@@ -19,7 +17,5 @@ route.get(
     renderizaCertificadoController,
 );
 
-
-// route.get('/certificado/html', (_req, res) => res.sendFile(resolve(__dirname, 'public', 'index.html')));
 
 module.exports = route;
